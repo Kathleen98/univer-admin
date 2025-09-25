6e1b7daf9a545cb76d3cdb1feaa64dc337928de1
Kathleen Santos <kathleen.santos@csacademy.com.br>
2025-09-25 20:40:05 -0300

    style: adding video inputs

--- src/components/AddContents/index.tsx
+++ src/components/AddContents/index.tsx
@@ -1,6 +1,7 @@
 import { Button } from "../ui/button"
 import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "../ui/dialog"
 import { Input } from "../ui/input"
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "../ui/select"
 
 export const AddContents = () => {
   return (
@@ -16,6 +17,34 @@ export const AddContents = () => {
         </DialogHeader>
         <DialogDescription>
           <Input type="text" placeholder="Título" />
+          <Select>
+            <SelectTrigger>
+              <SelectValue placeholder={'Categoria'} />
+            </SelectTrigger>
+
+            <SelectContent>
+              <SelectItem value="movie">Filmes</SelectItem>
+              <SelectItem value="serie">Série</SelectItem>
+            </SelectContent>
+          </Select>
+
+          <Input placeholder="Gênero" />
+          <Input placeholder="Duração" />
+
+          <Select>
+            <SelectTrigger>
+              <SelectValue placeholder={'Status'} />
+            </SelectTrigger>
+
+            <SelectContent>
+              <SelectItem value="active">Ativo</SelectItem>
+              <SelectItem value="new">Novidade</SelectItem>
+            </SelectContent>
+          </Select>
+
+          <Input placeholder="Data de lançamento" />
+
+          <Input placeholder="Descrição" />
         </DialogDescription>
       </DialogContent>
     </Dialog>
