05c61b954e16ad7c9d5b727dfe88ab872065a036
Kathleen Santos <kathleen.santos@csacademy.com.br>
2025-09-30 20:38:30 -0300

    feat: adding create video inputs

--- src/@types/zod/add-contents.ts
+++ src/@types/zod/add-contents.ts
@@ -2,12 +2,21 @@ import z from 'zod'
 
 export const SchemaAddContents = z.object({
   title: z.string(),
+  slug: z.string(),
   category: z.string(),
-  status: z.string(),
-  gender: z.string(),
+  description: z.string(),
   duration: z.string(),
   releaseDate: z.string(),
-  description: z.string()
+  videoUrl: z.string(),
+  thumbnailUrl: z.string(),
+  trailerUrl: z.string(),
+  introStartTime: z.number(),
+  introEndTime: z.number(),
+  ageRating: z.string(),
+  type: z.string(),
+  status: z.string(),
+  gender: z.string(),
+  isOriginal: z.boolean()
 })
 
 export type addContentData = z.infer<typeof SchemaAddContents>
--- src/components/AddContents/index.tsx
+++ src/components/AddContents/index.tsx
@@ -66,6 +66,7 @@ export const AddContents = () => {
               />
 
             </div>
+            <Input placeholder="Temporadas" />
             <Input placeholder="Epsódios" />
             <Input  {...register("gender")} placeholder="Gênero" />
             <Input  {...register("duration")} placeholder="Duração" />
